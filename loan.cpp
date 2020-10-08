46e35001c64675c412fc5ceee67fc310abffc102
Michael Bonnet <36342359+MichaelBonnet@users.noreply.github.com>
2020-10-08 13:38:24 -0500

    Add files via upload

---
+++ loan.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fstream;
+using std::ofstream;
+using std::ifstream;
+using std::ios;
+
